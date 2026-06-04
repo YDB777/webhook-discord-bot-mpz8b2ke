38c54da507d8ff7775f131fa870db1fc850554b1
YDB777 <austinramos033@gmail.com>
2026-06-04 03:13:07 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,19 +5,19 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 app.get('/', (req, res) => res.send('Bot is active!'));
-app.listen(port, '0.0.0.0',  () => console.log(`Server listening on port ${port}`));
+app.listen(port, '0.0.0.0', () => console.log(`Server listening on port ${port}`));
 
-// 
+//
 const client = new Client({ 
-  intents: [GatewayIntentBits.Guilds, 
-  });
+  intents: [GatewayIntentBits.Guilds] 
+});
 
-// 
+//
 const botId = "1511974725625053254"; 
 
 client.once('ready', () => {
   console.log(`Logged in successfully! Bot ID: ${botId}`);
 });
 
-// 
+//
 client.login(process.env.TOKEN);
