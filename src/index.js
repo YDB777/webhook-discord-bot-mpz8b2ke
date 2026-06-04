d9acb6d199461b74afbad63013b0a41dfce8dfe8
YDB777 <austinramos033@gmail.com>
2026-06-04 03:08:54 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
-process.on('uncaughtException', (err) => {
-  console.error('SYSTEM CRASHED ON STARTUP:', err);
+const { Client, GatewayIntentBits } = require('discord.js');
+const express = require('express');
+
+//
+const app = express();
+const port = process.env.PORT || 3000;
+app.get('/', (req, res) => res.send('Bot is active!'));
+app.listen(port, '0.0.0.0',  () => console.log(`Server listening on port ${port}`));
+
+// 
+const client = new Client({ 
+  intents: [GatewayIntentBits.Guilds, 
+  });
+
+// 
+const botId = "1511974725625053254"; 
+
+client.once('ready', () => {
+  console.log(`Logged in successfully! Bot ID: ${botId}`);
 });
 
-process.on('unhandledRejection', (reason, promise) => {
-  console.error('UNHANDLED REJECTION AT:', promise, 'REASON:', reason);
-}); 1511974725625053254
+// 
+client.login(process.env.TOKEN);
