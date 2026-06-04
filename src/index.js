5341fe2dbf0977e29530759f6b8916247ee53034
YDB777 <austinramos033@gmail.com>
2026-06-04 02:52:09 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ process.on('uncaughtException', (err) => {
 
 process.on('unhandledRejection', (reason, promise) => {
   console.error('UNHANDLED REJECTION AT:', promise, 'REASON:', reason);
-});
+}); 1511974725625053254
